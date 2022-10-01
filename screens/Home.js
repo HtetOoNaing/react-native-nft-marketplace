cd37a35324a9d8b2a4edf45366a6dfc7fb66dea9
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2022-10-01 23:30:15 +0630

    make upper background primary

--- screens/Home.js
+++ screens/Home.js
@@ -18,6 +18,19 @@ const Home = () => {
             ListHeaderComponent={<HomeHeader />}
           />
         </View>
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            zIndex: -1,
+          }}
+        >
+          <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+          <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+        </View>
       </View>
     </SafeAreaView>
   );
