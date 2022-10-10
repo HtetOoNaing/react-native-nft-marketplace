6789bd686bccdb4de64cd934d6a48f5fb6992532
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2022-10-10 20:31:00 +0630

    add button at bottom in details page

--- screens/Details.js
+++ screens/Details.js
@@ -7,7 +7,8 @@ import {
   StatusBar,
   FlatList,
 } from "react-native";
-import { FocusedStatusBar } from "../components";
+import { FocusedStatusBar, RectButton } from "../components";
+import { SHADOWS, SIZES } from "../constants";
 
 const Details = ({ route, navigation }) => {
   const { data } = route.params;
@@ -18,6 +19,20 @@ const Details = ({ route, navigation }) => {
         backgroundColor="transparent"
         translucent={true}
       />
+      <View
+        style={{
+          width: "100%",
+          position: "absolute",
+          bottom: 0,
+          paddingVertical: SIZES.font,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "rgba(255, 255, 255, 0.5)",
+          zIndex: 1
+        }}
+      >
+        <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark} />
+      </View>
     </SafeAreaView>
   );
 };
